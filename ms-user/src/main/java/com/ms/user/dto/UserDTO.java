ab282d9e85ae5ba1e5003ffdedefe114f5cb16e5
Adrian Noriega <noriega.adrian@gmail.com>
2023-11-13 18:33:20 -0600

    Creacion de entidad y DTO para usuario

---
+++ ms-user/src/main/java/com/ms/user/dto/UserDTO.java
@@ -0,0 +1,20 @@
+package com.ms.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+    private String document;
+
+    private String name;
+
+    private String email;
+
+    private String information;
+}
